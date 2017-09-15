4b757bd24435d09dad18c43dd409ba29f763c30c
Samuel Schepp <sa.schepp@googlemail.com>
2017-09-15 15:55:32 +0200

    Changed travis yaml

--- test.sh
+++
@@ -1,6 +0,0 @@
-xcodebuild test \
--scheme AugmentedCampusIosTests \
--workspace AugmentedCampusIos.xcodeproj/project.xcworkspace \
--sdk iphonesimulator \
-ONLY_ACTIVE_ARCH=NO \
--destination 'platform=iOS Simulator,name=iPhone 7,OS=11.0'
